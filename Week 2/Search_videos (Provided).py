aa4753b8d1a37558e98874abfe34a897239b2150
Daniel <dfzunigah@unal.edu.co>
2018-07-06 14:17:06 -0500

    Update Search_videos (Provided).py

--- Week 2/Search_videos (Provided).py
+++ Week 2/Search_videos (Provided).py
@@ -12,7 +12,7 @@ import unidecode
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyDI7zY0f-Ekc4MCGRy9NhTAuIGNG5CXkfc"
+DEVELOPER_KEY = "REPLACE ME"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
